4e1d7674bf687b0ae0d3ceae516413c6cfaff115
Társylla <contatotaymassoli@gmail.com>
2026-04-03 09:43:00 -0300

    anotação de try/execpt *importante revisar*

---
+++ estruturas_dados.py/anotação1.py
@@ -0,0 +1,21 @@
+# Testando LISTAS
+lista = ["carros",True, 2, 6.7]
+print(type(lista))
+print(lista)
+
+# Testando TUPLAS
+tupla = ("carro", True, 2, 6.7)
+print(type(tupla))
+print(tupla)
+
+# Testando DICIONÁRIOS
+dicionario = {'chave': 'valor'}
+print(dicionario)
+print(type(dicionario))
+
+# Testando CONJUNTOS
+conjuntos = {True, "carro", 4.5}
+print(conjuntos)
+print(type(conjuntos))
+
+# index não funciona em dicionários. começando pelo 0, não pelo 1
